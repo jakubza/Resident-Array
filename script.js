aa95f0f577c67d18a1eff7b6ca891404cba9a4dc
Matej Vavrinec <mvavrinec2008gmail.com>
2026-04-09 11:38:54 +0200

    Leon

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-const controlsBtn = document.getElementById('controls');
-const closeBtn = document.getElementById('closeControls');
-const overlay = document.getElementById('controlsOverlay');
+const controlsBtn = document.getElementById("controls");
+const closeBtn = document.getElementById("closeControls");
+const overlay = document.getElementById("controlsOverlay");
 
-controlsBtn.addEventListener('click', () => {
-    overlay.style.display = 'flex';
+ctx.imageSmoothingEnabled = false;
+
+controlsBtn.addEventListener("click", () => {
+  overlay.style.display = "flex";
 });
 
-closeBtn.addEventListener('click', () => {
-    overlay.style.display = 'none';
+closeBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
 });
 
-window.addEventListener('click', (event) => {
-    if (event.target == overlay) {
-        overlay.style.display = 'none';
-    }
+window.addEventListener("click", (event) => {
+  if (event.target == overlay) {
+    overlay.style.display = "none";
+  }
 });
+
+const playerImg = new Image();
+playerImg.src = "assets/Leon Kennedy.png";
+
+const player = {
+  x: 100,
+  y: 100,
+  width: 128,
+  height: 128
+};
 
 let gameRunning = false;
 
 function startGame() {
   document.getElementById("menu").style.display = "none";
-
   canvas.style.display = "block";
 
   gameRunning = true;
-  gameLoop();
+
+  if (playerImg.complete) {
+    gameLoop();
+  } else {
+    playerImg.onload = () => {
+      gameLoop();
+    };
+  }
 }
 
 function gameLoop() {
@@ -37,15 +55,21 @@ function gameLoop() {
   ctx.fillStyle = "green";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "red";
-  ctx.fillRect(100, 100, 50, 50);
+  ctx.drawImage(
+    playerImg,
+    player.x,
+    player.y,
+    player.width,
+    player.height
+  );
 
   requestAnimationFrame(gameLoop);
 }
+
 function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
